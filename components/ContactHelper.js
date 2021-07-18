7419c0710c1acbc716627ffae6b68ed571b42d10
CeIia <me@celiabourial.com>
2021-07-18 02:26:08 +0200

    :art: Externalized ContactHelper to its own component.

---
+++ components/ContactHelper.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+const ContactHelper = () => {
+  return (
+    <div className="mx-4 xl:mx-auto max-w-7xl py-14 my-10 rounded-xl bg-shamrock grid gric-cols-1 lg:grid-cols-2 gap-10 px-10 items-center text-white placeholder-white">
+      <div className="text-white">
+        <p className="uppercase text-sm mb-2">Une question ?</p>
+        <h2 className="text-4xl">
+          Nous sommes à votre disposition si vous avez la moindre question.
+        </h2>
+        <p className="mt-4 mb-2">Par téléphone au</p>
+        <Link href="tel:+33 6 78 90 12 34">
+          <a className="hover:underline text-xl font-bold flex items-center">
+            +33 6 78 90 12 34
+          </a>
+        </Link>
+      </div>
+      <form action="">
+        <div className="space-y-6">
+          <div>
+            <p className="uppercase text-sm mb-2">Votre email</p>
+            <input
+              type="email"
+              className="w-full bg-transparent placeholder-white border-b p-3"
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <p className="uppercase text-sm mb-2">Votre message</p>
+            <textarea
+              className="w-full h-24 bg-white bg-opacity-10 p-3 placeholder-white border-b border-white"
+              placeholder="J'ai une question à propos de ..."
+            ></textarea>
+          </div>
+          <div className="w-full">
+            <button className="float-right rounded bg-white bg-opacity-10 p-3 transition hover:bg-opacity-100 hover:text-shamrock">
+              Envoyer mon message
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactHelper;
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import { GoQuote } from "react-icons/go";
 import Advantage from "../components/HomeAdvantage";
 import Image from "next/image";
 import Footer from "../components/Footer";
+import ContactHelper from "../components/ContactHelper";
 
 export default function Home() {
   return (
@@ -134,44 +135,7 @@ export default function Home() {
       </div>
 
       {/* contact form */}
-      <div className="mx-4 xl:mx-auto max-w-7xl py-14 my-10 rounded-xl bg-shamrock grid gric-cols-1 lg:grid-cols-2 gap-10 px-10 items-center text-white placeholder-white">
-        <div className="text-white">
-          <p className="uppercase text-sm mb-2">Une question ?</p>
-          <h2 className="text-4xl">
-            Nous sommes à votre disposition si vous avez la moindre question.
-          </h2>
-          <p className="mt-4 mb-2">Par téléphone au</p>
-          <Link href="tel:+33 6 78 90 12 34">
-            <a className="hover:underline text-xl font-bold flex items-center">
-              +33 6 78 90 12 34
-            </a>
-          </Link>
-        </div>
-        <form action="">
-          <div className="space-y-6">
-            <div>
-              <p className="uppercase text-sm mb-2">Votre email</p>
-              <input
-                type="email"
-                className="w-full bg-transparent placeholder-white border-b p-3"
-                placeholder="Email"
-              />
-            </div>
-            <div>
-              <p className="uppercase text-sm mb-2">Votre message</p>
-              <textarea
-                className="w-full h-24 bg-white bg-opacity-10 p-3 placeholder-white border-b border-white"
-                placeholder="J'ai une question à propos de ..."
-              ></textarea>
-            </div>
-            <div className="w-full">
-              <button className="float-right rounded bg-white bg-opacity-10 p-3 transition hover:bg-opacity-100 hover:text-shamrock">
-                Envoyer mon message
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+      <ContactHelper />
 
       {/* footer */}
       <Footer />
