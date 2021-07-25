8017ea3f31ad1523087425a70ddb4b6eb079ca3c
CeIia <me@celiabourial.com>
2021-07-25 19:55:42 +0100

    :lipstick: Remove hover behavior

--- components/StarRating.js
+++ components/StarRating.js
@@ -7,25 +7,12 @@ const Star = ({ marked, starId }) => {
   );
 };
 
-const StarRating = ({ value }) => {
+const StarRating = ({ value, color }) => {
   const [rating, setRating] = React.useState(parseInt(value) || 0);
   const [selection, setSelection] = React.useState(0);
 
-  const hoverOver = (event) => {
-    let val = 0;
-    if (event && event.target && event.target.getAttribute("data-star-id"))
-      val = event.target.getAttribute("data-star-id");
-    setSelection(val);
-  };
   return (
-    <div
-      onMouseOut={() => hoverOver(null)}
-      onClick={(e) =>
-        setRating(e.target.getAttribute("data-star-id") || rating)
-      }
-      onMouseOver={hoverOver}
-      className="flex text-bali"
-    >
+    <div className={`flex text-{$color}`}>
       {Array.from({ length: 5 }, (v, i) => (
         <Star
           starId={i + 1}
