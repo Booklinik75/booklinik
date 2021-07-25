7836d7e1b20842a21ec74b2ef581f7dd74781d77
CeIia <me@celiabourial.com>
2021-07-25 19:58:29 +0100

    :sparkles: Add /destinations components

---
+++ components/DestinationHotel.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import StarRating from "./StarRating";
+import Link from "next/link";
+
+const DestinationHotel = ({ hotel, countrySlug, citySlug, city, country }) => {
+  return (
+    <div className="col-span-1 space-y-2">
+      <div className="w-full rounded-xl relative">
+        <div className="relative h-48 rounded-xl">
+          <Image
+            src="https://via.placeholder.com/1000?text=en+attente+d'image"
+            layout="fill"
+            objectFit="cover"
+            meta="TBD"
+            className="rounded-lg bg-gray-500"
+          />
+        </div>
+        <div className="absolute mx-4 text-white bottom-4 space-y-1">
+          <h3 className="text-lg">{hotel.name}</h3>
+          <div className="flex col-row gap-2 items-center text-xs">
+            <StarRating value={hotel.rating} color="white" />
+            &bull;
+            <p>{city}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-xs text-gray-600 text-justify mx-0.5">
+          {hotel.excerpt}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DestinationHotel;
---
+++ pages/destinations/index.js
@@ -0,0 +1,44 @@
+import ContactHelper from "../../components/ContactHelper";
+import Footer from "../../components/Footer";
+import Navigation from "../../components/Navigation";
+import PhotoBanner from "../../components/PhotoBanner";
+import Head from "next/head";
+import DestinationCountry from "../../components/DestinationCountry";
+
+export const getStaticProps = async (context) => {
+  const res = await fetch("http://localhost:8000/destinations");
+  const data = await res.json();
+
+  return {
+    props: {
+      destinations: data,
+    },
+  };
+};
+
+const DestinationsList = ({ destinations }) => {
+  return (
+    <div>
+      <Head>
+        <title>Booklinik | Les Destinations</title>
+      </Head>
+      <Navigation />
+      <PhotoBanner
+        fullWidth={true}
+        extraLarge={true}
+        title="Les Destinations"
+      />
+
+      <div className="mx-4 xl:mx-auto max-w-7xl">
+        {destinations.countries.map((country) => {
+          return <DestinationCountry country={country} key={country.id} />;
+        })}
+      </div>
+
+      <ContactHelper />
+      <Footer />
+    </div>
+  );
+};
+
+export default DestinationsList;
