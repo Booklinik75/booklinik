cdf685417898bd32fe32a0a9baf8ae09c46cc3cc
CeIia <me@celiabourial.com>
2021-08-05 01:25:59 +0200

    :sparkles: Add rooms configuration pages

---
+++ pages/dashboard/admin/rooms/add.js
@@ -0,0 +1,215 @@
+import DashboardUi from "../../../../components/DashboardUi";
+import { checkAdmin, getHotels } from "../../../../utils/ServerHelpers";
+import firebase from "../../../../firebase/clientApp";
+import { useRouter } from "next/router";
+import DashboardInput from "../../../../components/DashboardInput";
+import { useState, useEffect, useRef } from "react";
+import DashboardButton from "../../../../components/DashboardButton";
+import { doFileUpload } from "../../../../utils/ClientHelpers";
+import slugify from "slugify";
+import ProfileSelect from "../../../../components/ProfileSelect";
+import { useStateWithPromise } from "../../../../hooks/useStateWithPromise";
+
+export const getServerSideProps = async (ctx) => {
+  const userProfile = await checkAdmin(ctx);
+  const { slug } = ctx.query;
+  const hotels = await getHotels();
+
+  const hotelsOptions = [];
+
+  hotelsOptions.push({
+    value: "default",
+    label: "Sélectionner une option",
+  });
+
+  hotels.map((hotel) => {
+    hotelsOptions.push({ value: hotel.slug, label: hotel.name });
+  });
+
+  return {
+    props: {
+      hotelsOptions: hotelsOptions,
+    },
+  };
+};
+
+const AddRoom = ({ hotelsOptions }) => {
+  const router = useRouter();
+
+  const [isLoading, setLoading] = useState("idle");
+  const [progress, setProgress] = useState(0);
+
+  const [form, setFormData] = useState({
+    name: "",
+    slug: "",
+    extraPrice: 0,
+  });
+  const [images, setImages] = useState([]);
+
+  const [urls, setUrls] = useState([]);
+
+  const handleChange = (e) => {
+    if (e.target.name === "images") {
+      for (let i = 0; i < e.target.files.length; i++) {
+        const newImage = e.target.files[i];
+        newImage["id"] = Math.random();
+        setImages((prevState) => [...prevState, newImage]);
+      }
+    } else if (e.target.name === "name") {
+      form.slug = slugify(e.target.value, { lower: true });
+    } else if (e.target.type === "select-one") {
+      form.hotel = e.target.value;
+    }
+
+    setFormData({
+      ...form,
+
+      // Trimming any whitespace
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleUpload = async () => {
+    const promises = [];
+    let docData = {};
+
+    images.map((image) => {
+      const uploadTask = firebase
+        .storage()
+        .ref(`rooms/${image.name}`)
+        .put(image);
+      promises.push(uploadTask);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        (error) => {
+          console.log(error);
+        },
+        async () => {
+          await firebase
+            .storage()
+            .ref("rooms")
+            .child(image.name)
+            .getDownloadURL()
+            .then((urls) => {
+              console.log("coucou");
+              setUrls((prevState) => [...prevState, urls]);
+            });
+        }
+      );
+    });
+
+    Promise.all(promises)
+      .then(() => {})
+      .catch((err) => console.log(err));
+  };
+
+  const isInitialMount = useRef(true);
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      if (urls.length === images.length) {
+        doAdd();
+      } else if (urls.length > images.length) {
+        router.reload(window.location.pathname);
+      }
+    }
+  }, [urls]);
+
+  const doAdd = async (data) => {
+    setLoading("loading");
+
+    let docData = {
+      name: form.name,
+      slug: form.slug,
+      extraPrice: form.extraPrice,
+      photo: null,
+      hotel: form.hotel,
+      photos: urls,
+    };
+
+    console.log(docData);
+
+    firebase
+      .firestore()
+      .collection("rooms")
+      .add(docData)
+      .catch((error) => {})
+      .finally(() => {
+        setLoading("done");
+        setTimeout(() => {
+          setLoading("idle");
+        }, 1000);
+        router.push(`/dashboard/admin/rooms/edit/${form.slug}`);
+      });
+  };
+
+  return (
+    <DashboardUi isAdmin={true}>
+      <div className="col-span-10 space-y-4">
+        <h1 className="text-4xl">Ajouter une chambre d&apos;hôtel</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleUpload();
+          }}
+          className="space-y-4"
+        >
+          <DashboardInput
+            type="text"
+            name="slug"
+            value={form.slug}
+            onChange={handleChange}
+            disabled={true}
+            label="Slug"
+            required={true}
+          />
+          <DashboardInput
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+            disabled={false}
+            label="Nom"
+            required={true}
+          />
+          <DashboardInput
+            type="number"
+            name="extraPrice"
+            min={0}
+            value={form.extraPrice}
+            onChange={handleChange}
+            disabled={false}
+            label="Prix de la chambre"
+            required={true}
+          />
+          <ProfileSelect
+            label="Hotel"
+            name="hotel"
+            options={hotelsOptions}
+            value={form.hotel}
+            onChange={handleChange}
+          />
+          <DashboardInput
+            type="file"
+            name="images"
+            onChange={handleChange}
+            disabled={false}
+            label="Photos (plusieurs éléments autorisés)"
+            required={true}
+            multiple={true}
+          />
+          <DashboardButton defaultText="Ajouter" status={isLoading} />
+        </form>
+      </div>
+    </DashboardUi>
+  );
+};
+
+export default AddRoom;
---
+++ pages/dashboard/admin/rooms/index.js
@@ -0,0 +1,104 @@
+import DashboardUi from "../../../../components/DashboardUi";
+import {
+  checkAdmin,
+  getHotels,
+  getBackEndAsset,
+  getRooms,
+} from "../../../../utils/ServerHelpers";
+import Link from "next/link";
+import Image from "next/image";
+
+export const getServerSideProps = async (ctx) => {
+  const userProfile = await checkAdmin(ctx);
+  const hotels = await getHotels();
+  const rooms = await getRooms();
+
+  const resolveHotels = await Promise.all(
+    hotels.map(async (hotel, index, array) => {
+      let image = await getBackEndAsset(hotel.photo);
+      hotels[index].photo = image;
+    })
+  );
+  return {
+    props: { userProfile, hotels, rooms },
+  };
+};
+
+const RoomsList = ({ hotels, rooms }) => {
+  return (
+    <DashboardUi isAdmin={true}>
+      <div className="col-span-10 space-y-3">
+        <div className="flex w-full justify-between items-center">
+          <h1 className="text-4xl mb-4">Chambres</h1>
+          <Link href="rooms/add/" passHref={true}>
+            <button className="px-6 py-3 bg-shamrock text-white transition border rounded border-shamrock hover:bg-white hover:text-shamrock">
+              Ajouter
+            </button>
+          </Link>
+        </div>
+        <div className="space-y-6">
+          {hotels.map((hotel) => {
+            return (
+              <div key={hotel.slug} className="space-y-3">
+                <h2 className="text-xl mb-2">{hotel.name}</h2>
+                <div className="grid grid-cols-12 gap-4">
+                  {rooms.map((room) => {
+                    if (room.hotel === hotel.slug) {
+                      return (
+                        <div
+                          key={room.slug}
+                          className="col-span-12 md:col-span-6 lg:col-span-3"
+                        >
+                          <Link
+                            href={`/dashboard/admin/rooms/edit/${room.slug}`}
+                          >
+                            <a className="w-full lg:max-w-max">
+                              <div
+                                key={room.slug}
+                                className="rounded shadow bg-gray-50 transition hover:bg-gray-100 hover:shadow-lg hover:cursor-pointer"
+                              >
+                                <Image
+                                  src={room.photos[0]}
+                                  alt={room.name}
+                                  width={500}
+                                  height={250}
+                                  objectFit="cover"
+                                  className="rounded-t"
+                                />
+                                <div className="p-3 space-y-2">
+                                  <div className="flex flex-row items-center justify-between">
+                                    <p className="bg-gray-200 font-mono text-red-900 text-xs p-2 rounded max-w-max">
+                                      {room.slug}
+                                    </p>
+                                    <p className="bg-bali text-white px-2 py-1 rounded">
+                                      {room.extraPrice} €
+                                    </p>
+                                  </div>
+                                  <div className="gap-1">
+                                    <p>{room.name}</p>
+                                    <p className="text-xs text-gray-500">
+                                      {room.photos.length} photo
+                                      {room.photos.length > 0 ? "s" : ""}
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            </a>
+                          </Link>
+                        </div>
+                      );
+                    } else {
+                      return null;
+                    }
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </DashboardUi>
+  );
+};
+
+export default RoomsList;
