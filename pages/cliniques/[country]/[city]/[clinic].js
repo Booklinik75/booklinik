59eb94b794dc05333a6f66174f38d9631c6b890f
CeIia <me@celiabourial.com>
2021-08-16 18:59:15 +0200

    :sparkles: Add clinics views

---
+++ pages/cliniques/[country]/[city]/[clinic].js
@@ -0,0 +1,114 @@
+import Link from "next/link";
+import Navigation from "../../../../components/Navigation";
+import Footer from "../../../../components/Footer";
+import ContactHelper from "../../../../components/ContactHelper";
+import Head from "next/head";
+import Image from "next/image";
+import MDEditor from "@uiw/react-md-editor";
+import {
+  getBackEndAsset,
+  getCities,
+  getClinics,
+  getCountries,
+  getClinicData,
+} from "../../../../utils/ServerHelpers";
+
+export const getStaticPaths = async () => {
+  const countries = await getCountries();
+  const cities = await getCities();
+  const clinics = await getClinics();
+
+  const paths = [];
+
+  countries.map((country) => {
+    return cities.map((city) => {
+      return clinics.map((clinic) => {
+        return clinic.city === city.slug
+          ? paths.push({
+              params: {
+                country: country.slug,
+                city: city.slug,
+                clinic: clinic.slug,
+              },
+            })
+          : "";
+      });
+    });
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { country, city, clinic } = context.params;
+  const clinicData = await getClinicData(clinic);
+  const clinicPhoto = await getBackEndAsset(clinicData.props.data.photo);
+
+  return {
+    props: {
+      clinicData,
+      clinicPhoto,
+    },
+    revalidate: 120,
+  };
+};
+
+const ClinicPage = ({ clinicData, clinicPhoto }) => {
+  return (
+    <div className="space-y-6">
+      <Head>
+        <title>Booklinik | {clinicData.props.data.name}</title>
+      </Head>
+      <Navigation />
+      <div className="mx-4 space-y-10">
+        <div className="grid grid-cols-3 gap-6">
+          <div className="col-span-3 lg:col-span-1 bg-gray-100 p-14 space-y-4">
+            <h1 className="text-5xl">{clinicData.props.data.name}</h1>
+            <p>{clinicData.props.data.excerpt}</p>
+            <Link href="/book" passHref={true}>
+              <button className="text-white bg-shamrock rounded px-6 py-3 transition border border-shamrock hover:text-shamrock hover:bg-white">
+                Estimez mon séjour
+              </button>
+            </Link>
+          </div>
+          <div className="col-span-1 lg:col-span-2 w-full relative">
+            <Image
+              src={clinicPhoto}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center center"
+              alt="TBD"
+            />{" "}
+          </div>
+        </div>
+      </div>
+      <div className="mx-4 xl:mx-auto max-w-7xl space-y-10">
+        <div>
+          <MDEditor.Markdown source={clinicData.props.data.descriptionBody} />
+        </div>
+        {/* <div className="space-y-6">
+          <h2 className="text-2xl">Opérations similaires</h2>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
+            {relatedSurgeries.map((surgery) => {
+              return (
+                <RelatedElement
+                  title={surgery.name}
+                  target={`/operations/${surgery.category}/${surgery.slug}`}
+                  key={surgery.slug}
+                  picture={categoryPhoto}
+                />
+              );
+            })}
+          </div>
+        </div> */}
+      </div>
+      <ContactHelper />
+      <Footer />
+    </div>
+  );
+};
+
+export default ClinicPage;
---
+++ pages/cliniques/index.js
@@ -0,0 +1,70 @@
+import ContactHelper from "../../components/ContactHelper";
+import Footer from "../../components/Footer";
+import Navigation from "../../components/Navigation";
+import PhotoBanner from "../../components/PhotoBanner";
+import Head from "next/head";
+import ClinicCountry from "../../components/ClinicCountry";
+import {
+  getCities,
+  getCountries,
+  getClinics,
+  getBackEndAsset,
+} from "../../utils/ServerHelpers";
+
+export const getStaticProps = async (context) => {
+  const countries = await getCountries();
+  const cities = await getCities();
+  const clinics = await getClinics();
+
+  await Promise.all(
+    clinics.map(async (clinic, index) => {
+      let image = await getBackEndAsset(clinic.photo);
+      clinics[index].photo = image;
+    })
+  );
+
+  return {
+    props: {
+      countries,
+      cities,
+      clinics,
+    },
+  };
+};
+
+const ClinicsList = ({ countries, cities, clinics }) => {
+  const fileName =
+    "https://firebasestorage.googleapis.com/v0/b/booklinik.appspot.com/o/frontendassets%2Fsean-pollock-PhYq704ffdA-unsplash%20copie.jpg?alt=media&token=23a89001-8401-4f87-b16d-30f877cd3391";
+  return (
+    <div>
+      <Head>
+        <title>Booklinik | Les Cliniques</title>
+      </Head>
+      <Navigation />
+      <PhotoBanner
+        fileName={fileName}
+        fullWidth={true}
+        extraLarge={true}
+        title="Les Cliniques"
+      />
+
+      <div className="mx-4 xl:mx-auto max-w-7xl">
+        {countries.map((country) => {
+          return (
+            <ClinicCountry
+              country={country}
+              cities={cities}
+              key={country.id}
+              clinics={clinics}
+            />
+          );
+        })}
+      </div>
+
+      <ContactHelper />
+      <Footer />
+    </div>
+  );
+};
+
+export default ClinicsList;
