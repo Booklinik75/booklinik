b8665bcc5b552ffdd6711c876bcc2222484711dd
c11l <celia@hyperspread.com>
2021-10-09 21:59:19 +0200

    💄 Polish dashboard UI

--- components/bookingComponents/BookingUi.js
+++ components/bookingComponents/BookingUi.js
@@ -6,7 +6,7 @@ const BookingUi = ({ children, bookingData, step }) => {
     <div className="max-h-screen w-full overflow-hidden">
       <BookingTopNavigation bookingData={bookingData} />
       <div className="grid grid-cols-10">
-        <div className="col-span-2">
+        <div className="col-span-2 hidden md:block">
           <BookingSideNavigation step={step} />
         </div>
         <div className="col-span-8 shadow-xl h-full grid grid-cols-12 overflow-y-scroll">
