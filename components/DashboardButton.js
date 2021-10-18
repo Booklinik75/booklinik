4a3af4444dc1f6ec8540eab87413ad921e0afd0a
c11l <me@celiabourial.com>
2021-10-18 23:30:39 +0200

    ✨ Add payment varient to DashboardButton

--- components/DashboardButton.js
+++ components/DashboardButton.js
@@ -1,5 +1,6 @@
 import { BiLoaderAlt } from "react-icons/bi";
 import { AiOutlineCheck } from "react-icons/ai";
+import { FaCreditCard } from "react-icons/fa";
 
 const DashboardButton = ({
   defaultText,
@@ -7,19 +8,26 @@ const DashboardButton = ({
   onClick,
   className,
   disabled,
+  isPayment,
 }) => {
   let contents = "...";
 
   if (status === "loading") {
     contents = (
-      <div className="text-white animate-spin inline-block">
+      <div className="text-white animate-spin inline-block group-hover:text-shamrock">
         <BiLoaderAlt />
       </div>
     );
   } else if (status === "done") {
     contents = (
-      <div className="text-white inline-block animate-pulse">
+      <div className="text-white inline-block animate-pulse group-hover:text-shamrock">
         <AiOutlineCheck />
+      </div>
+    );
+  } else if (isPayment) {
+    contents = (
+      <div className="text-white flex gap-2 items-center justify-center group-hover:text-shamrock">
+        <FaCreditCard /> {defaultText}
       </div>
     );
   } else {
@@ -30,7 +38,7 @@ const DashboardButton = ({
     <button
       type="submit"
       className={`transition px-10 py-3 rounded border border-shamrock bg-shamrock text-white disabled:opacity-50 disabled:cursor-not-allowed disabled:col-span-full ${
-        !disabled && "hover:text-shamrock hover:bg-white"
+        !disabled && "hover:text-shamrock group hover:bg-white"
       } ${className}`}
       disabled={status === "loading" || disabled ? true : false}
       onClick={onClick}
