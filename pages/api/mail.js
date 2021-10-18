857f5617a8706d81f29bfc28d94b632c2b644109
c11l <me@celiabourial.com>
2021-10-18 23:28:58 +0200

    ✨ Add dynamic data support to email api

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -5,18 +5,18 @@ mail.setApiKey(process.env.SENDGRID_API_KEY);
 export default async (req, res) => {
   const body = JSON.parse(req.body);
 
-  const data = {
+  let data = {
     to: body.recipient,
     from: "info@booklinik.com",
     template_id: body.templateId,
     categories: ["Transactional"],
   };
 
-  if (body.attachments) {
-    data = { ...data, attachments: [...body.attachments] };
-  }
+  if (body.attachments) data = { ...data, attachments: [body.attachments] };
+  if (body.dynamicTemplateData)
+    data = { ...data, dynamicTemplateData: { ...body.dynamicTemplateData } };
 
-  await mail.send(data).catch((err) => console.log(err.response.body));
+  await mail.send(data).catch((err) => console.log(err));
 
   res.status(200).json({ status: "OK" });
 };
