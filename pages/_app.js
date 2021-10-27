8d867483c3e66e038fd6aa7cf26ee06832d52642
c11l <me@celiabourial.com>
2021-10-27 08:35:49 +0200

    ✨ Add crisp live chat

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import { AuthProvider } from "../utils/UserContext";
 import { ToastContainer } from "react-toastify";
@@ -14,34 +15,48 @@ import "react-dropdown/style.css";
 moment.locale("fr");
 
 function BooklinikClient({ Component, pageProps }) {
+  useEffect(() => {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "ab422553-9bcf-4ce4-ac32-d53b0d6e3b6b";
+    (() => {
+      const d = document;
+      const s = d.createElement("script");
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("body")[0].appendChild(s);
+    })();
+  });
+
   return (
-    <AuthProvider>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#33c783" />
-        <meta name="msapplication-TileColor" content="#33c783" />
-        <meta name="theme-color" content="#33c783" />
-      </Head>
-      <Component {...pageProps} />
-      <ToastContainer />
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#33c783" />
+          <meta name="msapplication-TileColor" content="#33c783" />
+          <meta name="theme-color" content="#33c783" />
+        </Head>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </AuthProvider>
+    </>
   );
 }
 
