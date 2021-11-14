a29cff1d5b40891a87805042fbe309cb17ad6600
ceIia <me@celiabourial.com>
2021-11-14 19:14:20 +0100

    💄 Add react-responsive-carousel css

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ import "moment/locale/fr";
 
 import "react-toastify/dist/ReactToastify.css";
 import "react-dropdown/style.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 moment.locale("fr");
 
