2df6da457ed46f2a66fb85983f4af9d3a9f732d8
faustabmnyh <faustaabimanyu@gmail.com>
2021-12-12 11:39:12 +0700

    handle add more surgery

--- components/editComponents/EditOperations.js
+++ components/editComponents/EditOperations.js
@@ -1,17 +1,50 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
-const EditOperations = ({ operation, operationCategories, setOperation }) => {
+const EditOperations = ({
+  operation,
+  operationCategories,
+  setOperations,
+  operations,
+}) => {
   const [openOperations, setOpenOperations] = useState(false);
   const handleChangeOperation = (getOp) => {
-    setOperation({
-      surgeryName: getOp.name,
-      surgeryPrice: getOp.startingPrice,
-      surgery: getOp.slug,
-      surgeryMinDays: getOp.minimumNights,
-      surgeryCategory: getOp.category,
-    });
+    const existOptions = operations.find(
+      (opt) => operation.surgery === opt.surgery
+    );
+    if (existOptions) {
+      setOperations((operations) =>
+        operations.map((opt) =>
+          opt.surgery === operation.surgery
+            ? {
+                surgeryName: getOp.name,
+                surgeryPrice: getOp.startingPrice,
+                surgery: getOp.slug,
+                surgeryMinDays: 0,
+                surgeryCategory: getOp.category,
+                cities: getOp.cities,
+              }
+            : opt
+        )
+      );
+    }
+
+    
+
     setOpenOperations(false);
+  };
+
+  console.log(operations);
+
+  const isSelected = (op) => {
+    return operations.find((oper) => oper.surgery === op.slug);
+  };
+
+  const handleRemove = () => {
+    setOperations((operations) =>
+      operations.filter((opt) => opt.surgery !== operation.surgery)
+    );
   };
 
   useEffect(() => {
@@ -26,17 +59,27 @@ const EditOperations = ({ operation, operationCategories, setOperation }) => {
 
   return (
     <div className="relative z-20">
-      <span
-        id="inputSurgery"
-        onClick={() => setOpenOperations((openOperations) => !openOperations)}
-        className="border p-2 py-3 px-4 rounded align-middle mx-2 border-shamrock cursor-pointer "
-        style={{
-          width: "fit-content",
-          minHeight: "30px",
-        }}
-      >
-        {operation.surgeryName}
-      </span>
+      <div className="flex items-center">
+        <span
+          id="inputSurgery"
+          onClick={() => setOpenOperations((openOperations) => !openOperations)}
+          className="border p-2 py-3 px-4 rounded align-middle mx-2 border-shamrock cursor-pointer "
+          style={{
+            width: "fit-content",
+            minHeight: "30px",
+          }}
+        >
+          {operation.surgeryName}
+        </span>
+        {operations.length > 1 && (
+          <span
+            className="bg-red-600 p-1 mr-2 rounded-full inline-block cursor-pointer"
+            onClick={handleRemove}
+          >
+            <FaTimes color="white" size="10" />
+          </span>
+        )}
+      </div>
       {openOperations && (
         <AnimatePresence>
           <motion.div
@@ -53,15 +96,18 @@ const EditOperations = ({ operation, operationCategories, setOperation }) => {
                 maxHeight: "10rem",
               }}
             >
-              {operationCategories.map((op) => (
-                <li
-                  key={op.id}
-                  onClick={() => handleChangeOperation(op)}
-                  className="p-3 py-2 w-100 hover:bg-gray-100"
-                >
-                  {op.name}
-                </li>
-              ))}
+              {operationCategories.map(
+                (op) =>
+                  !isSelected(op) && (
+                    <li
+                      key={op.id}
+                      onClick={() => handleChangeOperation(op)}
+                      className="p-3 py-2 w-100 hover:bg-gray-100"
+                    >
+                      {op.name}
+                    </li>
+                  )
+              )}
             </ul>
           </motion.div>
         </AnimatePresence>
