0dd5b1e028759757c5bf035e54e1d93bc327722d
faustabmnyh <faustaabimanyu@gmail.com>
2022-02-06 18:14:09 +0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -59,7 +59,19 @@ function BooklinikClient({ Component, pageProps }) {
 
       e.async = true;
       e.src = "https://cdn.weglot.com/weglot.min.js";
-
+      e.onload = () => {
+        Weglot.initialize({
+          api_key: "wg_48e609e9c8a8b4e4ecb5962b26f12a824",
+          switchers: [
+            {
+              location: {
+                target: ".language-switcher",
+                sibling: null,
+              },
+            },
+          ],
+        });
+      };
       t.parentNode.insertBefore(e, t);
     }
     handleWeglot(document, "script");
--- pages/_document.js
+++ pages/_document.js
@@ -21,23 +21,6 @@ export default class MyDocument extends Document {
           `,
             }}
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              Weglot.initialize({
-                api_key: "wg_48e609e9c8a8b4e4ecb5962b26f12a824",
-                switchers: [
-                  {
-                    location: {
-                      target: ".language-switcher",
-                      sibling: null,
-                    },
-                  },
-                ],
-              });
-          `,
-            }}
-          />
         </Head>
         <body>
           <Main />
