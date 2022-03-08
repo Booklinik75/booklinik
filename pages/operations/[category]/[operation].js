e46b440e6b06950196aeb3b42142cc5e847d8885
Molly <mollysarfati5@gmail.com>
2022-03-09 00:23:03 +0100

    replace markdown bug
    replace the <markdown> bug on operation page with a <pre>

--- pages/operations/[category]/[operation].js
+++ pages/operations/[category]/[operation].js
@@ -68,7 +68,7 @@ const OperationPage = ({ surgeryData, categoryPhoto, relatedSurgeries }) => {
             <p className="text-shamrock">
               À partir de {surgeryData.data.startingPrice}€
             </p>
-              <p>{surgeryData.data.excerpt}</p>
+            <pre>{surgeryData.data.excerpt} </pre>
             <Link href="/book" passHref={true}>
               <button className="text-white bg-shamrock rounded px-6 py-3 transition border border-shamrock hover:text-shamrock hover:bg-white">
                 Estimez mon séjour
