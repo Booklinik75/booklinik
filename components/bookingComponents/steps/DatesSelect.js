8199f46748a13afc169565358cb5701a0e5b23ef
Salah <salah.elbouhali@digityourdream.fr>
2022-08-04 11:36:37 +0200

    update date range

--- components/bookingComponents/steps/DatesSelect.js
+++ components/bookingComponents/steps/DatesSelect.js
@@ -15,15 +15,14 @@ const DatesSelectStep = ({
   addDays,
   setNextStep,
 }) => {
-  const disabledDays = new Array(
-    new Date(new Date().setDate(new Date().getDate() + 1)),
-    new Date(new Date().setDate(new Date().getDate() + 2))
-  );
+  
   const initialvalue = new Date();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
-  const [hideReturnCalendar, setHideReturnCalendar] = useState(true);
-  const [disabledDate, setDisabledDate] = useState(new Array());
+  const [focusRange, setFocusRange] = useState([]);
+  const [disabledDate, setDisabledDate] = useState(new Array(new Date(new Date().setDate(new Date().getDate() + 1)),
+  new Date(new Date().setDate(new Date().getDate() + 2)),
+  new Date(new Date().setDate(new Date().getDate() + 3))));
   const [dateCalendar, setDateCalendar] = useState([
     {
       startDate: new Date(),
@@ -31,6 +30,7 @@ const DatesSelectStep = ({
       key: "selection",
     },
   ]);
+  const test= {};
 
   useEffect(() => {
     // setNextStep to true when all inputs are filled
@@ -39,9 +39,10 @@ const DatesSelectStep = ({
     }
   }, [booking]);
   const PushData = (data) => {
-    setDisabledDate([...disabledDate, ...data]);
-    console.log(data + "=======in function=========/ ");
+    setDisabledDate([[], ...data]);
+  
   };
+
   const RemoveDisabledDate = () => {
     setDisabledDate([]);
   };
@@ -50,7 +51,9 @@ const DatesSelectStep = ({
     setStartDate(start);
     setEndDate(end);
     DisabledMinrange(start);
-    console.log(dates + "=======in onchange=========/ ");
+    onCalendarStartDateChange(start);
+    
+  
     function DisabledMinrange(data) {
       let celldisabled = [];
       var newday = new Date(start);
@@ -59,9 +62,21 @@ const DatesSelectStep = ({
         for (let i = 1; i < parseInt(booking.minimumNights); i++) {
           celldisabled.push(new Date(newday.setDate(newday.getDate() + 1)));
           console.log(i + "increment ==========date======");
+          
         }
-
-        end ? RemoveDisabledDate() : PushData(celldisabled);
+    
+     
+        PushData(celldisabled)
+      if (end){
+        let timeDiff = Math.abs(
+          end.getTime() - start.getTime()
+        );
+        let numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        onCalendarEndDateChange(end, numberOfNights);
+        RemoveDisabledDate() 
+      }
+     
+       ;
 
         console.log(celldisabled + "cell disabled ==========date======");
         console.log(new Date(start) + "date ==========newDate5start======");
@@ -72,6 +87,7 @@ const DatesSelectStep = ({
 
   return (
     <div className="space-y-6">
+      {}
       <h1 className="text-2xl mb-6">
         Choisissez vos dates de voyage
         {booking.totalSelectedNights !== 0 &&
@@ -98,14 +114,49 @@ const DatesSelectStep = ({
       </p>
       <div className="space-y-2">
         <div className="flex flex-col lg:flex-row gap-4">
-          {console.log(startDate + "--------startDate---------")}
-          {console.log(endDate + "--------endDate---------")}
+          {console.log(focusRange + "--------Focus---------")}
           <div className="w-full lg:w-1/2 xl:w-1/3 space-y-3">
+          <style>
+        {`.react-datepicker {
+  font-size: 1em;
+  font-family:helvetica;
+}
+.react-datepicker__header {
+  padding-top: 0.8em;
+}
+.react-datepicker__month {
+  margin: 0.4em 1em;
+}
+.react-datepicker__day-name, .react-datepicker__day {
+  width: 2.9em;
+  height:2.5em;
+  line-height: 1.9em;
+  margin: 0.166em;
+}
+.react-datepicker__day-names{
+  margin-top: 5px;
+}
+.react-datepicker__current-month {
+  font-size: 1.3em;
+}
+.react-datepicker__navigation {
+  top: 1em;
+  line-height: 1.7em;
+  border: 0.45em solid transparent;
+}
+.react-datepicker__navigation--previous {
+  left: 1em;
+}
+
+      }`}
+       </style>
             <h2 className="text-xs uppercase text-gray-500">
-        Date
-              de départ
+        Date de départ
             </h2>
             <DatePicker
+             locale="de-CH-1901"
+              moveRangeOnFirstSelection={true}
+              focusPlage={setFocusRange}
               selected={startDate}
               onChange={onChange}
               startDate={startDate}
