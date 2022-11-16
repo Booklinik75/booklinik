b1904ace20f5fefd339315a3fa2038c70ec3c7a6
Molly <mollysarfati5@gmail.com>
2022-11-16 17:43:55 +0100

    Update [operation].js

--- pages/operations/[category]/[operation].js
+++ pages/operations/[category]/[operation].js
@@ -6,6 +6,7 @@ import Head from "next/head";
 import Image from "next/image";
 import MDEditor from "@uiw/react-md-editor";
 import RelatedElement from "../../../components/RelatedSuggestionElement";
+import ReactCompareImage from 'react-compare-image';
 import {
   getBackEndAsset,
   getOperationCategories,
@@ -13,6 +14,8 @@ import {
   getRelatedSurgeries,
   getSurgeries,
   getSurgeryData,
+  getDoctorSurgeries,
+  getAfterBeforeSurgeries
 } from "../../../utils/ServerHelpers";
 
 export const getStaticPaths = async () => {
@@ -43,30 +46,35 @@ export const getStaticProps = async (context) => {
   const categoryData = await getOperationData(category);
   const categoryPhoto = await getBackEndAsset(categoryData.data.photo);
   const relatedSurgeries = await getRelatedSurgeries(surgeryData.data.category);
+  const doctorSurgeries= await getDoctorSurgeries(surgeryData.data.name)
+  const beforeAfter=await getAfterBeforeSurgeries(surgeryData.data.slug)
 
   return {
     props: {
       surgeryData,
       categoryPhoto,
       relatedSurgeries,
+      doctorSurgeries,
+      beforeAfter
     },
     revalidate: 120,
   };
 };
 
-const OperationPage = ({ surgeryData, categoryPhoto, relatedSurgeries }) => {
+const OperationPage = ({ surgeryData, categoryPhoto, relatedSurgeries, doctorSurgeries,beforeAfter }) => {
   return (
     <div className="space-y-6">
       <Head>
         <title>Booklinik | {surgeryData.data.name}</title>
       </Head>
       <Navigation />
+
       <div className="mx-4 space-y-10">
         <div className="grid grid-cols-3 gap-6">
           <div className="col-span-3 lg:col-span-1 bg-gray-100 p-14 space-y-4">
             <h1 className="text-5xl">{surgeryData.data.name}</h1>
             <p className="text-shamrock">
-              À partir de {surgeryData.data.startingPrice}€
+              À partir de {surgeryData.data.startingPrice} €
             </p>
             <pre>{surgeryData.data.excerpt} </pre>
             <Link href="/book" passHref={true}>
@@ -87,31 +95,107 @@ const OperationPage = ({ surgeryData, categoryPhoto, relatedSurgeries }) => {
         </div>
       </div>
       <div className="mx-4 xl:mx-auto max-w-7xl space-y-10">
+      {beforeAfter[0].beforeafter ?(
+       beforeAfter[0].beforeafter.length>0&&(
+       <div className="space-y-6">
+          <h2 className="text-2xl">Avant/Après</h2>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
+          {beforeAfter[0].beforeafter.map((x,i) => {
+
+              return (
+
+                  <div key={(x,i)} className="col-span-1 rounded relative transition shadow hover:shadow-lg group">
+                   <ReactCompareImage
+
+
+                  leftImage={x.leftimage}
+                  rightImage={x.rightimage} />
+
+                  </div>
+
+
+           )
+          })}
+
+
+
+
+        </div>
+
+        </div>
+
+
+
+     )) :(
+      ""
+    )}
+
         <div>
           <MDEditor.Markdown source={surgeryData.data.descriptionBody} />
         </div>
+        {
+       doctorSurgeries[0]?.doctor?.length>0?(
+        <div className="space-y-6">
+          <h2 className="text-2xl">Nos médecins</h2>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
+            {doctorSurgeries[0].doctor.map((x,i) => {
+
+              return (
+                <div  key={x.slug} className="">
+                <RelatedElement
+                  title={x.name}
+                  target="URL:void(0)"
+
+                  key={(x,i)}
+                  picture={x.photoUrl}
+              //  picture={surgeryData.data.photoUrl || categoryPhoto} // meme photo
+
+                />
+                <MDEditor.Markdown className="text-gray-600 overflow-ellipsis transition line-clamp-3 hover:line-clamp-none" source={x.doctorExcerpt}/>
+                </div>
+              );
+            })}
+          </div>
+
+
+
+        </div>
+
+
+
+        ) :(
+      ""
+    )}
+
+
+
+
         <div className="space-y-6">
           <h2 className="text-2xl">Opérations similaires</h2>
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
             {relatedSurgeries.map((surgery) => {
               return (
+
                 <RelatedElement
                   title={surgery.name}
                   target={`/operations/${surgery.category}/${surgery.slug}`}
                   key={surgery.slug}
                   picture={surgery.photoUrl || categoryPhoto}
-              //  picture={surgeryData.data.photoUrl || categoryPhoto} // meme photo
+                  //picture={surgeryData.data.photoUrl || categoryPhoto} // meme photo
 
                 />
+
               );
             })}
           </div>
-        </div>
-      </div>
-      <ContactHelper />
-      <Footer />
-    </div>
-  );
-};
 
+        </div>
+
+     </div>
+
+     <ContactHelper />
+     <Footer />
+   </div>
+ );
+};
 export default OperationPage;
