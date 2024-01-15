d4333d7e5b4e3610bed584ffa587f30909ab111b
Booklinik75 <141941539+Booklinik75@users.noreply.github.com>
2024-01-15 19:19:46 +0300

    Create weglo.js

---
+++ pages/dashboard/weglo.js
@@ -0,0 +1,19 @@
+import Script from 'next/script'
+ 
+export default function Dashboard() {
+  return (
+    <>
+
+<link rel="alternate" hreflang="fr" href="https://www.booklinik.com">
+<link rel="alternate" hreflang="en" href="https://en.booklinik.com">
+<link rel="alternate" hreflang="tr" href="https://tr.booklinik.com">
+<link rel="alternate" hreflang="es" href="https://es.booklinik.com">
+<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
+<script>
+    Weglot.initialize({
+        api_key: 'wg_48e609e9c8a8b4e4ecb5962b26f12a824'
+    });
+</script>
+  </>
+)
+}
