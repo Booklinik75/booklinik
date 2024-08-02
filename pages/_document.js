5570b0fec36354792e4496ba9a556505e2843222
salah93100 <salah.elbouhali@gmail.com>
2024-08-02 20:47:17 +0200

    add googlr Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,15 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <script
+            dangerouslySetInnerHTML={{
+              __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NGCD8B7');</script>`,
+            }}
+          />
+          <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=AW-11382787557`}
           />
@@ -27,7 +36,7 @@ export default class MyDocument extends Document {
           <NextScript />
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TB3SWNW"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGCD8B7"
             height="0" width="0" style="display:none; visibility:hidden" />`,
             }}
           />
