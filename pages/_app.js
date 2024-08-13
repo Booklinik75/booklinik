91d59ced34c993112a8134c571901a2aa30f849b
salah93100 <salah.elbouhali@gmail.com>
2024-08-13 14:47:57 +0200

    correct GTM Installation and clear old id

--- pages/_app.js
+++ pages/_app.js
@@ -112,8 +112,6 @@ function BooklinikClient({ Component, pageProps }) {
           ".language-switcher-container"
         );
 
-      
-
         if (window.location.pathname === "/") {
           Weglot.on("initialized", handleWeglotSwitcher);
         } else {
@@ -196,7 +194,7 @@ function BooklinikClient({ Component, pageProps }) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NXRNNN7L');
+          })(window,document,'script','dataLayer','GTM-NGCD8B7');
 
 `}{" "}
           </Script>
--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "node_modules/next/script";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NGCD8B7');</script>`,
-            }}
-          />
-        </Head>
+        <Head></Head>
         <body>
+          <Main />
+          <NextScript />
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGCD8B7"
             height="0" width="0" style="display:none; visibility:hidden" />`,
             }}
           />
-          <Main />
-          <NextScript />
         </body>
       </Html>
     );
